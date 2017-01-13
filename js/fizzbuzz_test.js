696c9f1dd5bd547f2c1df287af0ad7ae46b9e932
angkarn <angkarn9@gmail.com>
2017-01-13 12:06:05 +0700

    refactor test

--- js/fizzbuzz_test.js
+++ js/fizzbuzz_test.js
@@ -1,21 +1,16 @@
 describe("FizzBuzz", function() {
-    it("Input 1 Should be say 1", function() {
-        expect(FizzBuzz(1)).toEqual("1");
-    });
-
-    it("Input 2 Should Be say 2", function(){
+    it("should be say same input", function() {
         expect(FizzBuzz(2)).toEqual("2");
+        expect(FizzBuzz(4)).toEqual("4");
+        expect(FizzBuzz(1)).toEqual("1");
     });
 
     it("Input 3 Should Be say Fizz", function(){
         expect(FizzBuzz(3)).toEqual("Fizz");
     });
 
-    it("Input 4 Shoule Be say 4", function() {
-        expect(FizzBuzz(4)).toEqual("4");
-    });
-
     it("Input 5 Should Be say Buzz", function(){
         expect(FizzBuzz(5)).toEqual("Buzz");
     });
+
 });
