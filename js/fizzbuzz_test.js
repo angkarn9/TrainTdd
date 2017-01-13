b8ed86dfedc9fe9f7106c6fcf55e4cc2778208d1
angkarn <angkarn9@gmail.com>
2017-01-13 14:14:43 +0700

    Refactor fizzbuzz_test by set group

--- js/fizzbuzz_test.js
+++ js/fizzbuzz_test.js
@@ -10,31 +10,19 @@ describe("FizzBuzz", function() {
         expect(FizzBuzz(14)).toEqual("14");
     });
 
-    it("Input 3 Should Be say Fizz", function(){
+    it("Should Be say Fizz", function(){
         expect(FizzBuzz(3)).toEqual("Fizz");
-    });
-
-    it("Input 5 Should Be say Buzz", function(){
-        expect(FizzBuzz(5)).toEqual("Buzz");
-    });
-
-    it("Input 6 Should Be say Fizz", function(){
         expect(FizzBuzz(6)).toEqual("Fizz");
-    });
-
-    it("Input 9 Should Be say Fizz", function(){
         expect(FizzBuzz(9)).toEqual("Fizz");
-    });
-
-    it("Input 12 Should Be say Fizz", function(){
         expect(FizzBuzz(12)).toEqual("Fizz");
     });
 
-    it("Input 10 Should Be say Buzz", function(){
+    it("Should Be say Buzz", function(){
+        expect(FizzBuzz(5)).toEqual("Buzz");
         expect(FizzBuzz(10)).toEqual("Buzz");
     });
 
-    it("Input 15 Should Be say FizzBuzz", function(){
+    it("Should Be say FizzBuzz", function(){
         expect(FizzBuzz(15)).toEqual("FizzBuzz");
     });
 });
