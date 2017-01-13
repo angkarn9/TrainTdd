cb9779cacd813b3448546cffcab5b7c7695cc3ae
angkarn <angkarn9@gmail.com>
2017-01-13 14:48:15 +0700

    Refactor useless if

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,11 +1,13 @@
 function FizzBuzz(number){
-    return fizz(number) + buzz(number) || number.toString();
+    //return fizz(number) + buzz(number) || number.toString();
+    return fizzOrBuzz(number, 3) + fizzOrBuzz(number, 5) || number.toString();
 }
 
-function fizz(number){
-    return number % 3 === 0 ? "Fizz" : "";
+function fizzOrBuzz(number, key){
+    var fizzBuzzMap = {3:'Fizz', 5:'Buzz'};
+    return number % key === 0 ? fizzBuzzMap[key] : "";
 }
 
-function buzz(number){
+/*function buzz(number){
     return number % 5 === 0 ? "Buzz" : "";
-}
+}*/
